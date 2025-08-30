d383447961d0b0f1bc155af60928fd3f5df2d45a
Developer <user@example.com>
2025-08-29 22:41:24 -0300

    Update

--- Envie para Github.bat
+++ Envie para Github.bat
@@ -1,37 +1,7 @@
-@echo off
-color 0b
-:MENU
-cls
-echo ===============================
-echo       GITHUB AUTO PANEL
-echo ===============================
-echo 1 - Commit & Push (main)
-echo 2 - Status
-echo 3 - Log dos ultimos commits
-echo 4 - Sair
-echo ===============================
-set /p opc="Escolha uma opcao: "
-
-if "%opc%"=="1" goto COMMIT
-if "%opc%"=="2" goto STATUS
-if "%opc%"=="3" goto LOG
-if "%opc%"=="4" exit
-goto MENU
-
-:COMMIT
-set /p msg="Mensagem do commit: "
-git add .
-git commit -m "%msg%"
-git push origin main
+@echo off 
+echo Enviando projeto para o GitHub... 
+git add . 
+git commit -m "Update" 
+git push origin master 
+echo Projeto enviado com sucesso! 
 pause
-goto MENU
-
-:STATUS
-git status
-pause
-goto MENU
-
-:LOG
-git log --oneline -n 5
-pause
-goto MENU
